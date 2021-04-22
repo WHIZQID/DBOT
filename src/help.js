a804a497391b2a27844fae35712790bc5e2cc477
WHIZQID <76684226+WHIZQID@users.noreply.github.com>
2021-04-22 11:46:15 +0700

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,233 @@
+const help = (pushname, prefix, time) => {
+	return `  ●-▬▬▬ஜ۩۞۩ஜ▬▬▬▬-●
+
+
+────── ❮ *I N F O* ❯ ──────
+╭──────────────────╮
+│➢ *Nama* : ${pushname}
+│➢ *Lib* : BAILYES
+│➢ *Waktu* : ${time}
+╰──────────────────╯
+
+▬▭▬▭▬▭▬▭▬▭▬▭▬▭
+        *WHATSAPP BOT*
+▬▭▬▭▬▭▬▭▬▭▬▭▬▭
+
+╔════════════════════
+║          *SIMPLE MENU*
+╠════════════════════
+╠◪ *${prefix}sticker*
+╠◪ *${prefix}stickergif*
+╠◪ *${prefix}linesticker*
+╠◪ *${prefix}searchsticker*
+╠◪ *${prefix}toimg*
+╠◪ *${prefix}ocr*
+╠◪ *${prefix}tts text*
+╠◪ *${prefix}gtts text*
+╠◪ *${prefix}attp text*
+╠◪ *${prefix}img2url*
+╠◪ *${prefix}emoji2img*
+╠════════════════════
+║         *WHATSAPP BOT*
+╠════════════════════
+╠◪ *${prefix}menu*
+╠◪ *${prefix}info*
+╠◪ *${prefix}owner*
+╠◪ *${prefix}donasi*
+╠◪ *${prefix}runtime*
+╠◪ *${prefix}offcgroup*
+╠◪ *${prefix}bug Masalah*
+╠◪ *${prefix}blocklist*
+╠◪ *${prefix}wait*
+╠════════════════════
+║          *AGAMA MENU*
+╠════════════════════
+╠◪ *${prefix}quran*
+╠◪ *${prefix}quran1*
+╠◪ *${prefix}alkitab*
+╠◪ *${prefix}jsholat*
+╠◪ *${prefix}doawitir*
+╠◪ *${prefix}kisahnabi*
+╠◪ *${prefix}niatshalat*
+╠◪ *${prefix}wallmuslim*
+╠◪ *${prefix}quransurah*
+╠◪ *${prefix}randomquran*
+╠◪ *${prefix}bacaanshalat*
+╠════════════════════
+║      *DOWNLOADER MENU*
+╠════════════════════
+╠◪ *${prefix}play*
+╠◪ *${prefix}joox*
+╠◪ *${prefix}ytmp4*
+╠◪ *${prefix}ytmp3*
+╠◪ *${prefix}igtv*
+╠◪ *${prefix}igvideo*
+╠◪ *${prefix}igphoto*
+╠◪ *${prefix}mediafire*
+╠◪ *${prefix}tiktodmusik*
+╠◪ *${prefix}tiktoknowm*
+╠════════════════════
+║           *MEDIA MENU*
+╠════════════════════
+╠◪ *${prefix}tiktokstalk*
+╠◪ *${prefix}igstalk*
+╠◪ *${prefix}igsearch*
+╠◪ *${prefix}pinterest*
+╠◪ *${prefix}apkmod*
+╠◪ *${prefix}apkpure*
+╠◪ *${prefix}liputan*
+╠◪ *${prefix}foxnews*
+╠◪ *${prefix}tribunnews*
+╠◪ *${prefix}phcomment*
+╠◪ *${prefix}quotemaker*
+╠◪ *${prefix}resepmasakan*
+╠◪ *${prefix}nulis text*
+╠◪ *${prefix}nulis2 text*
+╠◪ *${prefix}nulis3 nama|kls|text*
+╠════════════════════
+║         *STORAGE MENU*
+╠════════════════════
+╠◪ *${prefix}fast*
+╠◪ *${prefix}bass*
+╠◪ *${prefix}blub*
+╠◪ *${prefix}toptt*
+╠◪ *${prefix}tupai*
+╠◪ *${prefix}ghost*
+╠◪ *${prefix}gemuk*
+╠◪ *${prefix}fasttesf*
+╠◪ *${prefix}slowmo*
+╠◪ *${prefix}slowmo1*
+╠◪ *${prefix}nightcore*
+╠════════════════════
+║         *OTHERS MENU*
+╠════════════════════
+╠◪ *${prefix}film*
+╠◪ *${prefix}nickff*
+╠◪ *${prefix}jam*
+╠◪ *${prefix}wiki*
+╠◪ *${prefix}bitly*
+╠◪ *${prefix}lirik*
+╠◪ *${prefix}chord*
+╠◪ *${prefix}cerpen*
+╠◪ *${prefix}cersex*
+╠◪ *${prefix}brainly*
+╠◪ *${prefix}quotes*
+╠◪ *${prefix}bpfont*
+╠◪ *${prefix}pantun*
+╠◪ *${prefix}artijodoh*
+╠◪ *${prefix}urlshort*
+╠◪ *${prefix}qrcode*
+╠◪ *${prefix}barcode*
+╠◪ *${prefix}pastebin*
+╠◪ *${prefix}simi text*
+╠◪ *${prefix}artimimpi*
+╠◪ *${prefix}infonomor*
+╠◪ *${prefix}memeindo*
+╠◪ *${prefix}mememaker*
+╠◪ *${prefix}happymod*
+╠◪ *${prefix}bacakomik*
+╠◪ *${prefix}jadwaltvnow*
+╠◪ *${prefix}watpaddsearch*
+╠════════════════════
+║           *RANDOM IMG*
+╠════════════════════
+╠◪ *${prefix}randombts*
+╠◪ *${prefix}randompubg*
+╠◪ *${prefix}randomwaifu*
+╠◪ *${prefix}randomshota*
+╠◪ *${prefix}randomhentai*
+╠════════════════════
+║           *LOGO MENU*
+╠════════════════════
+╠◪ *${prefix}3d*
+╠◪ *${prefix}tahta*
+╠◪ *${prefix}neon*
+╠◪ *${prefix}water*
+╠◪ *${prefix}phlogo*
+╠◪ *${prefix}leaves*
+╠◪ *${prefix}coffe1*
+╠◪ *${prefix}coffe2*
+╠◪ *${prefix}logoff*
+╠◪ *${prefix}matrix*
+╠◪ *${prefix}thunder*
+╠◪ *${prefix}romance*
+╠◪ *${prefix}firework*
+╠◪ *${prefix}wolflogo*
+╠◪ *${prefix}fakeserti*
+╠◪ *${prefix}sparkling*
+╠◪ *${prefix}darkneon*
+╠◪ *${prefix}breakwall*
+╠◪ *${prefix}glitchtext*
+╠◪ *${prefix}tweetfake*
+╠◪ *${prefix}goldbutton*
+╠◪ *${prefix}silverbutton*
+╠◪ *${prefix}anakharam*
+╠◪ *${prefix}ffserti*
+╠◪ *${prefix}ffserti1*
+╠◪ *${prefix}ffserti2*
+╠◪ *${prefix}ffserti3*
+╠◪ *${prefix}ffserti4*
+╠◪ *${prefix}mltserti*
+╠◪ *${prefix}mltserti2*
+╠◪ *${prefix}mltserti3*
+╠◪ *${prefix}mltserti4*
+╠◪ *${prefix}mltserti5*
+╠◪ *${prefix}pubgserti*
+╠◪ *${prefix}pubgserti1*
+╠◪ *${prefix}pubgserti2*
+╠◪ *${prefix}pubgserti3*
+╠◪ *${prefix}pubgserti4*
+╠════════════════════
+║           *GAME MENU*
+╠════════════════════
+╠◪ *${prefix}slot*
+╠◪ *${prefix}slot1*
+╠◪ *${prefix}koin*
+╠◪ *${prefix}truth*
+╠◪ *${prefix}dare*
+╠◪ *${prefix}fitnah*
+╠◪ *${prefix}jadian*
+╠◪ *${prefix}wa.me*
+╠◪ *${prefix}readmore*
+╠◪ *${prefix}tebakgambar*
+╠════════════════════
+║           *GROUP MENU*
+╠════════════════════
+╠◪ *${prefix}delete*
+╠◪ *${prefix}add*
+╠◪ *${prefix}kick*
+╠◪ *${prefix}clone*
+╠◪ *${prefix}leave*
+╠◪ *${prefix}tagall*
+╠◪ *${prefix}tutup*
+╠◪ *${prefix}kickall*
+╠◪ *${prefix}hidetag*
+╠◪ *${prefix}promote*
+╠◪ *${prefix}demote*
+╠◪ *${prefix}listonline*
+╠◪ *${prefix}listadmins*
+╠◪ *${prefix}linkgroup*
+╠◪ *${prefix}opentime*
+╠◪ *${prefix}closetime*
+╠◪ *${prefix}groupinfo*
+╠◪ *${prefix}mentionall*
+╠◪ *${prefix}ownergroup*
+╠◪ *${prefix}group open|close*
+╠◪ *${prefix}antilink enable/disable*
+╠◪ *${prefix}welcome enable/disable*
+╠════════════════════
+║       *OWNER COMAND*
+╠════════════════════
+╠◪ *${prefix}bc*
+╠◪ *${prefix}setcr*
+╠◪ *${prefix}return*
+╠◪ *${prefix}clearall*
+╠◪ *${prefix}setprefix*
+╠◪ *${prefix}setleave*
+╠◪ *${prefix}setwelcome*
+╠════════════════════
+║ *CREATOR MR D1XZ*
+╚════════════════════`
+}
+
+exports.help = help
